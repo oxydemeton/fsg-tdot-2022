1124d394703585efb44dfcfa796adc5cdea70e61
M B <mablasteam@gmail.com>
2022-10-17 19:23:48 +0200

    create basic svelte app with tailwind in frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="">
+  Hello World
+  <Counter></Counter>
+</main>
+
+<style>
+
+</style>
---
+++ frontend/src/lib/Counter.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment} class="border-8 border-black">
+  count is {count}
+</button>
+
+<style>
+</style>
