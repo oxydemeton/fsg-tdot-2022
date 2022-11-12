0963d073c9b8e263a3d7856955aa6a5ad17b108e
M B <mablasteam@gmail.com>
2022-11-12 16:50:26 +0100

    add theme color

--- frontend/src/Header.svelte
+++ frontend/src/Header.svelte
@@ -9,6 +9,7 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="description" content="Schülerführung für neue Schüler am FSG für den 03.12.2022 am Tag der offenen Tür.">
     <meta name="application-name" content="Fsg Tdot guide 2022">
+    <meta name="theme-color" content="#2E2E2E">
     <meta name="generator" content="svelte">
 
 </svelte:head>
