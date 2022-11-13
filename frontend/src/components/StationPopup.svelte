94d911d0355b478c66cd1403efbaa1aca73f7b5d
M B <mablasteam@gmail.com>
2022-11-13 13:44:10 +0100

    disigned dialog

--- frontend/src/components/StationPopup.svelte
+++ frontend/src/components/StationPopup.svelte
@@ -22,16 +22,13 @@
         @apply outline-0;
     }
     .submit-btn {
-        @apply rounded-lg bg-neutral-400 w-3/4 p-0.5 my-1 border-2 border-neutral-800 font-semibold text-xl;
+        @apply rounded-lg bg-neutral-400/80 w-full p-0.5 my-1 border-2 border-neutral-800 font-semibold text-xl;
     }
     form {
-        @apply w-1/4 h-auto bg-neutral-500/60 rounded-xl mt-1 p-4;
-    }
-    .center-form {
-        @apply text-center;
+        @apply w-1/3 h-auto bg-neutral-500/70 rounded-xl mt-1 p-4;
     }
     dialog {
-        @apply fixed top-0 left-0 flex justify-center items-center h-screen w-screen bg-neutral-500/50 z-20 backdrop-blur-sm select-none;
+        @apply fixed top-0 left-0 flex justify-center items-center h-screen w-screen bg-neutral-600/50 z-20 backdrop-blur-sm select-none;
     }
     h2 {
         @apply text-3xl font-bold font-mono w-full text-center;
@@ -40,19 +37,20 @@
 <dialog open>
     <form on:submit={submit}>
         <button type="button" on:click={()=>dispatch("close")} class="
-            rounded-xl text-xl bg-white w-7 h-7 font-extrabold font-mono hover:ring-4 ring-fsg text-center">
+            rounded-xl text-xl bg-black/50 w-7 h-7 font-extrabold font-mono hover:ring-4 ring-fsg text-center">
             X
         </button>
-        <div class="center-form">
+        <div class="text-center items-center p-4">
             <h2>{station.name}</h2>
             {#if (station.solution.length > 0)}
                 <input required type="text" name="solution" id="stationsolution" maxlength={station.solution.length} spellcheck="false" bind:value={txt} placeholder="Lösung"
-                    class="w-full my-1 rounded block
-                    text-white
-                    bg-neutral-600/75 focus:bg-neutral-800/90
-                    font-light focus:font-medium
-                    placeholder-gray-300 placeholder:italic placeholder:font-light
-                    focus:ring-4 ring-fsg
+                    autocapitalize="none" autocomplete="off"
+                    class="w-full my-1 rounded-lg block px-1 py-0.5
+                        text-white
+                        bg-neutral-600/75 focus:bg-neutral-800/90
+                        font-light focus:font-medium
+                        placeholder-gray-300 placeholder:italic placeholder:font-light
+                        focus:ring-4 ring-fsg
                     ">
                 <button type="submit" class="submit-btn">
                     Check
