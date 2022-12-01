2a655ac7d06700894962e94b4e4acd24420a4716
M B <mablasteam@gmail.com>
2022-12-01 16:25:44 +0100

    added colors used on the maps

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -7,7 +7,13 @@ module.exports = {
         'fsg': "#3e6fb4",
         'main': "#303033",
         'dark': "#111113",
-        'light': "#ABABAD"
+        'light': "#ABABAD",
+        "map-green": "#4A7B3D",
+        "map-red": "#A80000",
+        "map-purple": "#7030A0",
+        "map-gray": "#7F7F7F",
+        "map-orange": "#7030A0",
+        "map-blue": "#406BB8"
       },
       minWidth: {
         '1/2': '50%',
