4a4f203c79ebdfaa09c033bbfe8b1f4d3d2ad61b
M B <mablasteam@gmail.com>
2022-12-01 15:21:07 +0100

    created spoiler component

---
+++ frontend/src/components/Spoiler.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let label = "Anzeigen"
+    export let disabled = false
+    let show = false
+</script>
+
+<style>
+
+</style>
+
+<div class="spoiler">
+    {#if (show)}
+        <button on:click={()=>show=true} type="button" disabled={disabled}>{label}</button>
+    {:else}
+        <slot></slot>
+    {/if}
+</div>
