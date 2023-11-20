dc35e7ec7fe7ea3da4adf7e9390f0341633142f8
M B <mablasteam@gmail.com>
2023-11-20 16:55:14 +0100

    removed prefetches

--- frontend/src/Header.svelte
+++ frontend/src/Header.svelte
@@ -1,35 +1,9 @@
 <script lang="ts">
     export let station: string
-    import Eg               from "./assets/map/eg.png"
-    import Ug               from "./assets/map/ug.png"
-    import Og               from "./assets/map/og.png"
-    import LockClosed       from "./assets/SchlossNormal.png"
-    import LockOpen         from "./assets/SchlossGrün.png"
-    import LockSelected     from "./assets/SchlossRot.png"
-    import Reset            from "./assets/reset.png"
-    import ArrowRed         from "./assets/arrow_red.svg"
-    import ArrowGray        from "./assets/arrow_gray.svg"
-    import QR               from "./assets/qrcode.png"
-    const images = [
-        Eg,
-        Og,
-        Ug,
-        LockClosed,
-        LockOpen,
-        LockSelected,
-        Reset,
-        ArrowRed,
-        ArrowGray,
-        QR,
-    ]
 </script>
 
 <svelte:head>
     <title>Station: {station}</title>
-
-    {#each images as pre_img}
-        <link rel="prefetch" href={pre_img} as="image">
-    {/each}
 
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
