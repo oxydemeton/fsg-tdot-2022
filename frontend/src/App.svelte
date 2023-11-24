48cc5d212a524dde7451b8e580a720315bd731da
M B <mablasteam@gmail.com>
2023-11-24 23:22:33 +0100

    comment out console logs

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -8,7 +8,7 @@
     import InfoIcon from "./assets/info.svg"
     import InfoScreen from "./components/InfoScreen.svelte";
     //Loop to log the station order of each group
-    for (let gr = 0; gr < group_count; gr++) {
+    /*for (let gr = 0; gr < group_count; gr++) {
         const stations_in_order: string[] = []
         for (let st = 0; st < all_stations.length; st++) {
             stations_in_order.push(all_stations[id_by_group_and_num(gr, st)].name)
@@ -19,7 +19,7 @@
         })
         console.log(`Group ${gr}`, stations_str);
         
-    }
+    }*/
     let floor = 0
     let group: number = -1
     let station_num = 0
